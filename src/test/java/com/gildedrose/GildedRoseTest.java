68b3625d7b1dd1f0f1c25312c8dc20e2cef071ec
sabire <hasankorpe07@gmail.com>
2026-01-07 15:58:12 +0100

    Test1: normal item decreases sellIn and quality

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,13 +6,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+//    @Test
+//    void foo() {
+//        Item[] items = new Item[] { new Item("foo", 0, 0) };
+//        GildedRose app = new GildedRose(items);
+//        app.updateQuality();
+//        assertEquals("fixme", app.items[0].name);
+//    }
+@Test
+void normalItem_degradesTwiceAsFastAfterSellDate() {
+    Item[] items = {
+            new Item("Normal Item", 0, 10)
+    };
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+
+    assertEquals(-1, items[0].sellIn);
+    assertEquals(8, items[0].quality);
+}
+
 
 
 
